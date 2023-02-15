4e22edbe65f11420c55a279639b9be594a305a0f
Steven Joseph <stevolisisjoseph@gmail.com>
2023-02-15 12:03:57 +0100

    first commit2

--- authentication/admin-auth.php
+++ authentication/admin-auth.php
@@ -3,18 +3,19 @@
 	use Firebase\JWT\JWT;
 	use Firebase\JWT\Key;
 	$JWT_SECRET = "secure_coding";
-///
+
     $response = new stdClass();
 
-	if (isset($_GET['cookie'])) {
+	if (isset($_COOKIE['adminPass2'])) {
 	try{
 		$JWT_SECRET = "secure_coding";
-		$cookie = $_GET['cookie'];
+		$cookie = $_COOKIE['adminPass2'];
 		$decodeJWT = JWT::decode($cookie,new Key($JWT_SECRET, 'HS256'));
 
-		$response->decodeJWT = $decodeJWT;
-		$response->status ='success';
-		header("HTTP/1.1 200");
+		$response->decodeJWT = $y;
+		$response->decodeJWT2 = $cookie;
+		$response->status = 'success';
+// 		header("HTTP/1.1 200");
 		echo json_encode($response);
 		
 	}catch(Exception $e){
@@ -24,10 +25,9 @@
 		echo json_encode($response);
 
 	}
-
- }else{
- 		$response->status = $_COOKIE;
+	}else{
+	    $response->status = 'no cookie';
 		header("HTTP/1.1 404");
-		echo json_encode($response);	
- }
+		echo json_encode($response);
+	}
 ?>
